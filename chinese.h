efe1ae8c70e498861b1ac4404c50e1621038706f
harrylzh <harrylzh@hotmail.com>
2021-11-22 10:21:47 +0800

    first commit

---
+++ chinese.h
@@ -0,0 +1,81 @@
+#ifndef CHINESE_H
+#define CHINESE_H
+/* header begin */
+  
+#define  包含 include
+#define  标准输入输出库 <stdio.h>
+#define  布尔逻辑库 <stdbool.h>
+
+
+#define  主函数 main
+#define  返回 return
+#define  开始 {
+#define  结束 }
+
+
+
+// #define  不能修改 const
+#define  常量 const
+#define  布尔数 bool
+#define  真 true
+#define  假 false
+#define  整数 int
+#define  实数 float
+#define  字符 char
+#define  字符串指针 char *
+#define  字符串(c) char c[1024] 
+#define  空 void
+#define  整数数组(c,l) int (c)[(l)] 
+#define  数组项(c,l) (c)[(l)-1]
+#define  枚举 enum 
+
+
+
+#define  将 
+#define  如果 if
+#define  否则 else
+#define  循环 for
+#define  循环正序次数i(c) for(int 当前次数i = 1; 当前次数i  <= c ; 当前次数i++)
+#define  循环正序次数j(c) for(int 当前次数j = 1; 当前次数j  <= c ; 当前次数j++)
+#define  循环正序次数k(c) for(int 当前次数k = 1; 当前次数k  <= c ; 当前次数k++)
+#define  执行 do
+#define  当 while
+#define  判断 switch
+#define  当等于 case
+#define  跳出 break
+#define  继续 continue 
+
+#define  设为 =
+#define  等于 ==
+#define  不等于 !=
+#define  大于 >
+#define  大于等于 >=
+#define  小于 <
+#define  小于等于 <=
+
+#define  加 +
+#define  减 -
+#define  乘 *
+#define  除 /
+#define  余数 %
+
+
+
+
+
+
+#define  输出 printf
+#define  输出整数(c) printf("%d", c)
+#define  输出字符串(c) printf("%s", c)
+#define  输出字符(c) printf("%c", c)
+#define  输出空格 printf("%c",' ')
+#define  换行 printf("\n")
+#define  输入整数(c)   scanf("%d", &c)
+#define  输入字符   getchar()
+
+
+#define  正常退出 0
+#define  换行符号 '\n'
+#define  字符串结束符号 '\0'
+/* header end */
+#endif
